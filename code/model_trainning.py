d5f7075ce6a1b3e8e4b92f281a3dc8e96bce94c4
mli0603 <lizhaoshuo12345@126.com>
2019-05-14 08:10:03 -0400

    added notebook to show failure and success case

--- code/model_trainning.py
+++ code/model_trainning.py
@@ -86,8 +86,12 @@ def validate(model,device,dice_loss,num_class,validation_generator,validation_da
     tp = torch.zeros(num_class)
     fp = torch.zeros(num_class)
     fn = torch.zeros(num_class)
-    worst_dice = 1.0
-    best_dice = 0.0
+   
+    worst_dice = np.ones((5,1))
+    best_dice = np.zeros((5,1))
+    
+    worst_batch = [0,0,0,0,0]
+    best_batch = [0,0,0,0,0]
     
     for i_batch, batch in enumerate(validation_generator):
         # read img and label
@@ -125,12 +129,15 @@ def validate(model,device,dice_loss,num_class,validation_generator,validation_da
         curr_dice = ((2*curr_tp + 1e-7)/ (2*curr_tp+curr_fp+curr_fn+1e-7)).mean()
         
         # find best and worst
-        if worst_dice > curr_dice:
-            worst_batch = batch
-            worst_dice = curr_dice
-        if best_dice < curr_dice:
-            best_batch = batch
-            best_dice = curr_dice
+        if np.max(worst_dice) > curr_dice:
+            idx = np.argmax(worst_dice)
+            worst_batch[idx] = batch
+            worst_dice[idx] = curr_dice
+            
+        if np.min(best_dice) < curr_dice:
+            idx = np.argmin(best_dice)
+            best_batch[idx] = batch
+            best_dice[idx] = curr_dice
         
         if debug:
             break
@@ -282,4 +289,69 @@ def run_training(model,device,num_class,scheduler,optimizer,dice_loss,num_epochs
             print('-' * 10)
             
     return best_model_wts, best_acc
+    
+    
+def validate_and_compare(model1,model2,device,dice_loss1,dice_loss2,num_class,validation_generator,validation_dataset):
+    ########################### Validation #####################################
+    model1.eval()  # Set model to validation mode   
+    model2.eval()  # Set model to validation mode   
+    
+    tp = torch.zeros(num_class)
+    fp = torch.zeros(num_class)
+    fn = torch.zeros(num_class)
+    
+    worst_dice = np.zeros((5,1))
+    best_dice = np.zeros((5,1))
+    
+    worst_batch = [0,0,0,0,0]
+    best_batch = [0,0,0,0,0]
+    
+    for i_batch, batch in enumerate(validation_generator):
+        # read img and label
+        img = batch['img']
+        label = batch['label']
+        indx = batch['indx']
+        
+        if debug:
+            # validate if images are parsed correctly
+            print(i_batch, img.shape, label.shape)
+            sample_img = img[0,:,:,:]
+            sample_label = label[0,:,:,:]
+            sample_colorlabel = validation_dataset.label_converter.label2color(sample_label.permute(1,2,0))
+            imshow(sample_img.permute(1,2,0),denormalize=True)
+            imshow(sample_colorlabel)
+        
+        # transfer to GPU
+        img, label = img.to(device), label.to(device)
+
+        # forward
+        outputs1 = model1(img)
+        outputs2 = model2(img)
+        # get loss
+        _, probas1, true_1_hot = dice_loss1.forward(outputs1, label.long())
+        _, probas2, true_1_hot = dice_loss2.forward(outputs2, label.long())
+        
+        curr_tp1, curr_fp1, curr_fn1 = label_accuracy(probas1.cpu(),true_1_hot.cpu())
+        curr_tp2, curr_fp2, curr_fn2 = label_accuracy(probas2.cpu(),true_1_hot.cpu())
+        
+        curr_dice1 = ((2*curr_tp1 + 1e-7)/ (2*curr_tp1+curr_fp1+curr_fn1+1e-7)).mean()
+        curr_dice2 = ((2*curr_tp2 + 1e-7)/ (2*curr_tp2+curr_fp2+curr_fn2+1e-7)).mean()
+        
+        diff_dice = curr_dice2-curr_dice1
+        
+        # find best and worst
+        if np.max(worst_dice) > diff_dice:
+            idx = np.argmax(worst_dice)
+            worst_batch[idx] = batch
+            worst_dice[idx] = diff_dice
+            
+        if np.min(best_dice) < diff_dice:
+            idx = np.argmin(best_dice)
+            best_batch[idx] = batch
+            best_dice[idx] = diff_dice
+        
+        if debug:
+            break
+    
+    return [worst_batch,worst_dice],[best_batch,best_dice]
     
